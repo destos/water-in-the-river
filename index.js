ccbb3b0a749248ff1dbddf680db3493b07cecec1
Patrick Forringer <patrick@forringer.com>
2015-06-18 22:00:19 -0500

    some cleanup

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/', function(request, response) {
   //
   // 2. Information from Keystone (http://www.swt-wc.usace.army.mil/webdata/gagedata/KEYO2.current.html)
   // The two important columns are "TW-ELEV FEET GOES" and "RELEASE CFS POWERQ"
-  // They should both increase at the same time, and whenever they do you will see a change in the Tulsa area 6 hours later. 
+  // They should both increase at the same time, and whenever they do you will see a change in the Tulsa area 6 hours later.
   //
   //
   // 3. Generation Schedules (http://www.swpa.gov/generationschedules.aspx) and (http://www.swpa.gov/gen/fri.htm)
@@ -23,14 +23,14 @@ app.get('/', function(request, response) {
   // SWPA will release the generation schedule about 4pm for the next day.
   // 3 KEY is keystone, up until the flood it would say either 35 or 70, with 35 resulting in 5,000 cfs or 2' in the river and 70 resulting in 10,000 cfs or 4' in the river. It now shows 50 and I have no idea why.
   // The values can change, at some point a person died below grand lake dam, which resulted in them turning it off while they looked for them. To make up for that, Keystone turned on. The charts aren't updated.
-  // Again, ~6 hours after they say they will generate, riverside will start seeing water. 
+  // Again, ~6 hours after they say they will generate, riverside will start seeing water.
 
   output = "<h1>";
   Promise.all([key(),gauge()]).then(function(res){
-    gaugeLevel = res[1];
-    if (res[1] >= 2.) {
+    var gaugeLevel = res[1];
+    if (gaugeLevel >= 2.0) {
       output += "Yes.";
-    } else if (res[1] >= 1.) {
+    } else if (gaugeLevel >= 1.0) {
       output += "A little.";
     } else if (res[2]) {
       console.log(res[2]);
